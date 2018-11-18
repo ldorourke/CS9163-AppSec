fc3311c1f05ac8c6070ac37cae6aac15e976d8c1
Nikhil Padhye <nikhilpadhye2@gmail.com>
2018-11-18 18:07:27 -0500

    New logging with db functionality

--- DjangoAppSec/appsec/admin.py
+++ DjangoAppSec/appsec/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-
+from models import AuditEntr
 # Register your models here.
 from .models import User
 
 admin.site.register(User)
+
+@admin.register(AuditEntry)
+class AuditEntryAdmin(admin.ModelAdmin):
+    list_display = ['action', 'username', 'ip',]
+    list_filter = ['action',]
--- DjangoAppSec/appsec/models.py
+++ DjangoAppSec/appsec/models.py
@@ -2,6 +2,8 @@
 from django import forms
 from django.db import models
 from django.forms import ModelForm
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -10,3 +12,30 @@ from django.forms import ModelForm
 class User(models.Model):
 	uname = models.CharField(max_length = 128)
 	hashed_pass = models.CharField(max_length = 128)
+	action = models.CharField(max_length=64)
+    ip = models.GenericIPAddressField(null=True)
+	
+	def __unicode__(self):
+		return '{0} - {1} - {2}'.format(self.action, self.uname, self.ip)
+
+    def __str__(self):
+        return '{0} - {1} - {2}'.format(self.action, self.uname, self.ip)
+
+
+@receiver(user_logged_in)
+def user_logged_in_callback(sender, request, user, **kwargs):  
+    ip = request.META.get('REMOTE_ADDR')
+    AuditEntry.objects.create(action='user_logged_in', ip=ip, uname=user.uname)
+
+
+@receiver(user_logged_out)
+def user_logged_out_callback(sender, request, user, **kwargs):  
+    ip = request.META.get('REMOTE_ADDR')
+    AuditEntry.objects.create(action='user_logged_out', ip=ip, uname=user.uname)
+
+
+@receiver(user_login_failed)
+def user_login_failed_callback(sender, credentials, **kwargs):
+    AuditEntry.objects.create(action='user_login_failed', uname=credentials.get('username', None))
+
+
